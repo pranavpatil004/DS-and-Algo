2113e44a876c5c047a15d43a5e22c5a297b56089
pranavpatil004 <pranavpatil004@gmail.com>
2022-03-16 22:51:27 +0530

    added default

--- Python Basics/test.py
+++ Python Basics/test.py
@@ -1,6 +1,12 @@
 
 
+def test(t=1, **kwargs):
+    print(t)
+    print(kwargs)
+
 if __name__ == '__main__':
     print('This program is being run by itself')
 else:
     print('I am being imported from another module')
+
+print(test(a=2, b=3))
