8cd50b9405562f68aa86cf83e53b9fdec2a1d9a6
Pranav Patil <pranavpatil@Pranavs-MacBook-Pro.local>
2022-05-06 21:56:01 +0530

    small changes in factory

--- LLD/Factory/Factory Method/auto_factory.py
+++ LLD/Factory/Factory Method/auto_factory.py
@@ -1,12 +1,16 @@
 import autos
-
+from inspect import isclass, isabstract, getmembers
 class AutoFactory:
+    def __init__(self):
+        self.autos_dic = {}
+        self.load_autos()
+    def load_autos(self):
+        classes = getmembers(autos, lambda m: isclass(m) and not isabstract(m))
+        for name, _type in classes:
+            if isclass(_type) and issubclass(_type, autos.AbstractAuto):
+                self.autos_dic[name] = _type
     def get_auto(self, vehical_type):
-        if vehical_type == "Ford":
-            return autos.ford_fusion.FordFusion()
-        elif vehical_type == "Chevy":
-            return autos.chevy_volt.ChevyVolt()
-        elif vehical_type == "Jeep":
-            return autos.jeep_sahara.JeepSahara()
+        if vehical_type in self.autos_dic:
+            return self.autos_dic[vehical_type]()
         else:
             raise NotImplementedError("Vehical type not recognized", vehical_type)
--- LLD/Factory/Factory Method/main.py
+++ LLD/Factory/Factory Method/main.py
@@ -1,7 +1,7 @@
 from auto_factory import AutoFactory
 if __name__ == "__main__":
     factory = AutoFactory()
-    for vehical in ["Ford", "Jeep", "Chevy"]:
+    for vehical in ["FordFusion", "JeepSahara", "ChevyVolt"]:
         car = factory.get_auto(vehical)
         car.start()
         car.stop()
